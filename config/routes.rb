3d86c957999013c53031de9dade04f96096b4692
Claire Murray <claire.e.murray00@gmail.com>
2018-01-01 10:03:58 -0500

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :long_urls
+    root 'long_url#index'
+    # get '/longlinks'=>'longlinks#index'
+    get "/:id" => 'long_url#show'
 end
